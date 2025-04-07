96d4f5ebd580fa7f76b8f185262e9e597241cca6
anita <anita.rend@gmail.com>
2025-04-07 19:58:59 +0200

    exception exercise

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        boolean unos = false;
+
+        while (!unos) {
+            System.out.println("Upisite: ");
+
+            try {
+                String tekst = input.nextLine();
+                int broj = Integer.parseInt(tekst);
+                System.out.println(broj);
+                unos = true;
+            } catch (Exception e) {
+                System.out.println("Unesen krivi format broja!");
+            }
+        }
+        input.close();
+    }
 }
